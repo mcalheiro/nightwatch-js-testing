b2623c1e16a4c2b03cdf04d7627824be8777d952
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2024-09-21 16:36:40 +0100

    Add test hooks

--- test/home.spec.js
+++ test/home.spec.js
@@ -1,21 +1,27 @@
 describe('Home page', function() {
-    it('Should have the correct title', function(browser) {
-        browser.navigateTo('/').assert.textEquals('h1', 'Introducing Nightwatch v3')
+
+    beforeEach(function(browser) {
+        browser.navigateTo('/').window.maximize()
     })
 
-    it('Should lead the the installation page when click on Get Started', function (browser) {
-        browser.navigateTo('/')
+    afterEach(function(browser) {
+        browser.end()
+    })
+
+    it('Should have the correct title', function(browser) {
+        browser.assert.textEquals('h1', 'Introducing Nightwatch v3')
+    })
+
+    it('Should lead the the installation page when click on Get Started', function(browser) {
         browser.element.findByText('Get Started').click()
         browser.element.findByPlaceholderText('Filter by title').waitUntil('visible')
         browser.element.find('h1').getText().assert.equals('Install Nightwatch')
         browser.assert.titleEquals('Getting Started | Developer Guide | Nightwatch.js')
         browser.assert.urlContains('nightwatchjs.org/guide/quickstarts')
         browser.element.findByPlaceholderText('Filter by title').getAttribute('autocomplete').assert.equals('off')
-        browser.end()
     })
 
-    it('Should allow search and show correct results', function (browser) {
-        browser.navigateTo('/')
+    it('Should allow search and show correct results', function(browser) {
         browser.element.find('#docsearch').click()
         browser.element.find('.DocSearch-Modal').waitUntil('visible')
 
@@ -25,6 +31,5 @@ describe('Home page', function() {
         browser.element.find('.DocSearch-Dropdown-Container').assert.present()
         searchInput.sendKeys([browser.Keys.ARROW_DOWN, browser.Keys.ENTER])
         browser.element.find('h1').getText().assert.contains('.frameParent')
-        browser.end()
     })
 })
