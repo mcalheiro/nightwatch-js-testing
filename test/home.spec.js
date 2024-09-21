8dcb735352a28221a609c2c1de96dea3b7e44cef
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2024-09-21 18:56:22 +0100

    Update test to use pages

--- test/home.spec.js
+++ test/home.spec.js
@@ -1,3 +1,5 @@
+const guide = require("../pages/guide");
+
 describe('Home page', function() {
 
     before(function (browser) {
@@ -6,7 +8,8 @@ describe('Home page', function() {
     })
 
     beforeEach(function (browser) {
-        browser.navigateTo('/').window.maximize()
+        browser.window.maximize()
+        browser.page.home().navigate()
     })
 
     afterEach(function (browser) {
@@ -18,12 +21,11 @@ describe('Home page', function() {
     })
 
     it('Should lead the the installation page when click on Get Started', function (browser) {
-        browser.element.findByText('Get Started').click()
-        browser.element.findByPlaceholderText('Filter by title').waitUntil('visible')
-        browser.element.find('h1').getText().assert.equals('Install Nightwatch')
-        browser.assert.titleEquals('Getting Started | Developer Guide | Nightwatch.js')
-        browser.assert.urlContains('nightwatchjs.org/guide/quickstarts')
-        browser.element.findByPlaceholderText('Filter by title').getAttribute('autocomplete').assert.equals('off')
+        browser.page.home().getStarted()
+        const guidePage = browser.page.guide()
+        guidePage.waitLoading()
+        guidePage.basicAssertions()
+        guidePage.section.sidebar.element.find('@text_filter').getAttribute('autocomplete').assert.equals('off')
     })
 
     it('Should allow search and show correct results', function (browser) {
@@ -55,7 +57,7 @@ describe('Home page', function() {
             document.querySelector('header .navigation-list').style.display = 'none'
             document.querySelector('header .navigation__logo').style.width = '900px'
         }, [changeText])
-        browser.pause(4000)
+        browser.pause(1000)
         browser.element.findByText('{Client Side Execution}').click()  
         browser.assert.titleMatches('Getting Started')      
     })
